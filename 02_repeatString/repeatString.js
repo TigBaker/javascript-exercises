c9d2c44addcbb0882e292e2e10603f394e7a536c
TigBaker <18632373+TigBaker@users.noreply.github.com>
2023-11-14 19:24:31 +1300

    add javascript code to meet test specs
    For each module added the javascript needed and checked by running the
    'npm test name.spec.js' file.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, repeats) {
+    let result = "";
 
+    if (repeats < 0) {
+        return "ERROR";
+    }
+
+    for (let i =0; i < repeats; i++) {
+        result += str;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+    if (min < 0 || max < 0) {
+            return "ERROR";
+    }
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";        
+    }
+    let sum = 0;
 
+    for (let index = min; index <= max; index++) {
+        sum += index;        
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
